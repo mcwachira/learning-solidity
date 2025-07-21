5f4af1130a25f2f6502cb5cf1b0a6b375a1bcbab
mcwachira <mcwachira@outlook.com>
2025-07-21 20:53:12 +0300

    Created a new contract simple storage

--- contracts/SimpleStorage.sol
+++ contracts/SimpleStorage.sol
@@ -1,9 +1,9 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.2;
+pragma solidity ^0.8.24; //solidity version
 
 contract SimpleStorage {
     // initialized top zero if no value is given
-    uint256  myFavoriteNumber; //0
+    uint256 myFavoriteNumber; //0
     // uint254[] listOfFavoriteNumbemyFs;
 
     // types are created using struct
@@ -16,22 +16,21 @@ contract SimpleStorage {
     //dynamic array
     Person[] public listOfPeople;
 
-
-//mapping type
-mapping(string => uint256) public nameToFavoriteNumber;
+    //mapping type
+    mapping(string => uint256) public nameToFavoriteNumber;
 
     function store(uint256 _favoriteNumber) public {
         myFavoriteNumber = _favoriteNumber;
     }
 
     //view, pure
+    //view  is used to indicated a function  that can only read state but does not modify it
     function retrieve() public view returns (uint256) {
         return myFavoriteNumber;
     }
 
     //calldata, memory, storgae
     function addPerson(string memory _name, uint256 _favoriteNumber) public {
- 
         listOfPeople.push(Person(_favoriteNumber, _name));
         nameToFavoriteNumber[_name] = _favoriteNumber;
     }
